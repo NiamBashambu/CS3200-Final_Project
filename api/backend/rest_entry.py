45c9eb015837595e0b8d3dff576e6bcd11c2318b
Niam Bashambu <niambashambu@icloud.com>
2024-11-24 16:51:35 -0500

    adding resume routes to rest entry

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -5,6 +5,7 @@ from backend.db_connection import db
 from backend.posts.post_routes import posts
 from backend.employer.employer_routes import employer
 from backend.jobListing.jobListing_routes import jobListing
+from backend.resume.resume_routes import resume
 import os
 from dotenv import load_dotenv
 
@@ -42,7 +43,8 @@ def create_app():
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(posts,    url_prefix='/p')
     app.register_blueprint(employer, url_prefix = '/e')
-    app.register_blueprint(jobListing, url_preix = '/j')
+    app.register_blueprint(jobListing, url_prefix = '/j')
+    app.register_blueprint(resume,url_prefix = '/r')
 
     # Don't forget to return the app object
     return app
