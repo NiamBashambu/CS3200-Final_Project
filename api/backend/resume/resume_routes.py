4dff6f5612f6a70e25ad3d2f013bafe50afec07a
pallavishankarr <pshankar6996@gmail.com>
2024-11-24 16:50:05 -0500

    Created routes for resume

---
+++ api/backend/resume/resume_routes.py
@@ -0,0 +1,34 @@
+########################################################
+# Sample customers blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+
+
+# Create a new Blueprint object for resume
+resume = Blueprint('resume', __name__)
+
+# ------------------------------------------------------------
+# Get resumes for all existing students
+@resume.route('/resume', methods=['GET'])
+def get_all_resume():
+    query = '''
+        SELECT ResumeId, StudentId, Content, LastUpdated 
+        FROM Resume
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
+    
