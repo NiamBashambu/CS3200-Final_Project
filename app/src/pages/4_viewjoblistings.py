27c5f14d5bad1cd0d757048ae6aad1e4575d915e
ngynethan <nguyenethan150@gmail.com>
2024-12-01 00:52:46 -0500

    job listing spelling typo fix

--- app/src/pages/4_viewjoblistings.py
+++ app/src/pages/4_viewjoblistings.py
@@ -39,7 +39,7 @@ st.markdown("</div>", unsafe_allow_html=True)
 # Display user's first name if authenticated
 if st.session_state.get("authenticated"):
     user_name = st.session_state.get("name", "Guest")
-    st.write(f"Hello, {user_name}! 👋 Welcome to the JobListing Feed")
+    st.write(f"Hello, {user_name}! 👋 Welcome to the Job Listing Feed")
 
 # Fetch Job Listings Automatically from API
 try:
