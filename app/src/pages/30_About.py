987d9f5ef2579bccb890d1777284c3ec4f252104
ngynethan <nguyenethan150@gmail.com>
2024-12-01 21:41:13 -0500

    working on about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -4,13 +4,29 @@ from modules.nav import SideBarLinks
 
 SideBarLinks()
 
-st.write("# About this App")
+st.title('**Co-Connect**')
+st.write('Your go-to platform for Co-op opportunities!')
+
+st.subheader("About Us")
 
 st.markdown (
     """
-    This is CO-Connect. 
-    A Facebook like app for CO-OPs. 
+    We are *Co-Connect*, a Facebook like app for Co-ops. <br>
 
-    Stay tuned for more information and features to come!
-    """
-        )
+    Our platform addresses the difficulties and frustrations felt by students 
+    and employers in the co-op process. Co-ops offer students valuable 
+    opportunities to jumpstart their careers in their chosen fields. However, 
+    due to the limited information provided in job descriptions, students often
+    find themselves realizing too late--once they're already on the job--that 
+    the role doesn't meet their needs or expectations. <br>
+
+    NUWorks, the current platform, lacks features such as workload variability, 
+    work environment, realistic responsibilities, and much more. Northeastern 
+    students need a platform that offers a realistic insight into the co-op world 
+    and the industries they are preparing to enter. *Co-Connect* offers a more 
+    intuitive, data-driven platform that is tailored to co-ops offered to Northeastern
+    students. <br>
+
+    Check out some of our features below!
+    """,
+        unsafe_allow_html=True)
